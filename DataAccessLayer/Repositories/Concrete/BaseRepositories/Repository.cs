196750536fcc34b2be5944f64a4d5bf4277d0cd5
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-02-11 14:48:20 +0300

    11.02 / BaseRepository -1

---
+++ DataAccessLayer/Repositories/Concrete/BaseRepositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLayer.Repositories.Interface.BaseRepositories;
+using EntityLayer.Entities.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Concrete.BaseRepositories
+{
+    public abstract class Repository<T> : IBaseRepositories<T> where T : class, IBaseEntity// => "IRepository"'de yazdığımız methodlara burada gövde kazandıracağız ve abstract olarak işaretlediğim "BaseRepository" sınıfını child sınıflarda çağıracağım.
+    {
+        public Task Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Get(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetListAll(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/Interface/BaseRepositories/IBaseRepositories.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Entities.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Interface.BaseRepositories
+{
+    // Repository: Temel olarak veritabanı sorgulama işlemlerinin bir merkezden yapılmasını sağlar kod tekrarını önler.
+    public interface IBaseRepositories<T> where T : class, IBaseEntity
+    {
+        Task insert(T t);
+        Task Delete(T t);
+        Task Update(T t);
+        Task<List<T>> GetAll(); // Asenkron programlama yapmak istediğimiz methodlarımızı "TASK" olarak işaretlenir.
+        Task<List<T>> GetListAll(Expression<Func<T, bool>> filter);  //filter 
+        Task Get(Expression<Func<T, bool>> filter);  //dışarıdann bir şart alıcak
+
+        //Task<bool> Any(Expression<Func<T, bool>> expression);
+        Task<T> FirstOrDefault(Expression<Func<T, bool>> filter);
+        Task GetById(int id);
+
+
+  
+    }
+}
