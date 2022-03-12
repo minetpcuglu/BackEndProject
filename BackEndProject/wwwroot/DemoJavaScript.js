d09f8b5dd0d9162f536c6f2b01b08442ef853837
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-03-12 18:49:08 +0300

    12.03 / Ajax Dinamic Delete

---
+++ BackEndProject/wwwroot/DemoJavaScript.js
@@ -0,0 +1,16 @@
+﻿function Delete(id) {
+    if (confirm("Silmek istediğinize emin misiniz ?")) {
+        $.ajax({
+            type: "POST",
+            url: "/Hobby/DeleteHobby/" + id,
+            contentType: "application/json; charset=utf-8",
+
+            dataType: "json",
+            success: function (func) {
+
+                alert("Yazar Silme işlemi başarılı bir şekilde gerçekleştirildi");
+            }
+
+        });
+    }
+};
